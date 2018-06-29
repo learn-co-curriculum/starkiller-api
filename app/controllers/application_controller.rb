996dc077f1628eef98b01e1b1bf0c46b0e9f7f62
github username <github email address>
2018-06-29 15:01:58 -0400

    added text to root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+  def index
+    render json: "Welcome to the StarKiller API! There's so many API goodies! Go to /colleges or /acapella_groups."
+  end
+
 end
