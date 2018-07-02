6c70991348dff1063f81b09928538008f3c33026
nikymorg <nicole.morgan@gmail.com>
2018-07-02 17:12:15 -0400

    update root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
 
   def index
-    render json: "Welcome to the StarKiller API!"
+    render html: "<h1>Welcome to the StarKiller API!</h1><p>For documentation on endpoints, please visit <a href='https://github.com/learn-co-curriculum/starkiller-api' target=_blank>here.</a></p>".html_safe
   end
 
 end
